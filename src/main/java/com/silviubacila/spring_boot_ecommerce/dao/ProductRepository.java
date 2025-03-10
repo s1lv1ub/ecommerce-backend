baeab87d5f8dbff8be3ab5a19765908dcd103715
s1lv1u <silviu.bacila.adrian@gmail.com>
2025-03-10 22:58:55 +0200

    search for product by text box

--- src/main/java/com/silviubacila/spring_boot_ecommerce/dao/ProductRepository.java
+++ src/main/java/com/silviubacila/spring_boot_ecommerce/dao/ProductRepository.java
@@ -15,4 +15,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
 }
